7e890e4753b0126ad8b54f89db09e731f4d6947c
jackietana <213204740+jackietana@users.noreply.github.com>
2025-09-05 13:39:55 +0300

    add service

--- internal/service/logger.go
+++ internal/service/logger.go
@@ -1,1 +1,30 @@
 package service
+
+import (
+	"context"
+
+	logger "github.com/jackietana/grpc-logger/pkg/domain"
+)
+
+type LoggerRepository interface {
+	Insert(ctx context.Context, item logger.LogItem) error
+}
+
+type LoggerService struct {
+	repo LoggerRepository
+}
+
+func NewLoggerService(repo LoggerRepository) *LoggerService {
+	return &LoggerService{repo}
+}
+
+func (ls *LoggerService) Insert(ctx context.Context, req *logger.LogRequest) error {
+	item := logger.LogItem{
+		Action:    req.GetAction().String(),
+		Entity:    req.GetEntity().String(),
+		EntityID:  req.GetEntityId(),
+		Timestamp: req.GetTimestamp().AsTime(),
+	}
+
+	return ls.repo.Insert(ctx, item)
+}
