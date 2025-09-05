540efdbe270e6f34916797479b7b0d51bf2a16d4
jackietana <213204740+jackietana@users.noreply.github.com>
2025-09-05 13:40:57 +0300

    add server

--- internal/server/logger.go
+++ internal/server/logger.go
@@ -1,1 +1,26 @@
 package server
+
+import (
+	"context"
+
+	logger "github.com/jackietana/grpc-logger/pkg/domain"
+)
+
+type LoggerService interface {
+	Insert(ctx context.Context, req *logger.LogRequest) error
+}
+
+type LoggerServer struct {
+	logger.UnimplementedLoggerServiceServer
+	service LoggerService
+}
+
+func NewLoggerServer(service LoggerService) *LoggerServer {
+	return &LoggerServer{service: service}
+}
+
+func (ls *LoggerServer) Log(ctx context.Context, req *logger.LogRequest) (*logger.Empty, error) {
+	err := ls.service.Insert(ctx, req)
+
+	return &logger.Empty{}, err
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,38 @@
 package server
+
+import (
+	"fmt"
+	"net"
+
+	logger "github.com/jackietana/grpc-logger/pkg/domain"
+	"google.golang.org/grpc"
+)
+
+type Server struct {
+	grpcSrv      *grpc.Server
+	loggerServer logger.LoggerServiceServer
+}
+
+func New(loggerServer logger.LoggerServiceServer) *Server {
+	return &Server{
+		grpcSrv:      grpc.NewServer(),
+		loggerServer: loggerServer,
+	}
+}
+
+func (s *Server) ListenAndServe(port int) error {
+	addr := fmt.Sprintf(":%d", port)
+
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		return err
+	}
+
+	logger.RegisterLoggerServiceServer(s.grpcSrv, s.loggerServer)
+
+	if err := s.grpcSrv.Serve(lis); err != nil {
+		return err
+	}
+
+	return nil
+}
